70f0ba0163a0dc391664fbdcb26cb30a56877f86
Sergei Zhuravlev <worthless@MacBook-Pro-Sergei.local>
2023-09-11 14:43:13 +0300

    fix_photo

---
+++ TelegramBot/Commands/Matching.py
@@ -0,0 +1,26 @@
+# from Service.TelegramUserService import TelegramUserService as tus
+
+
+
+# def start_match(my_id, other_tags):
+
+
+
+
+
+
+
+#     final_data = {}
+#     list_of_dicts = [users]
+#     data = tus.GetSimilarTgUser(user_id.from_user.id)
+#     for user_first in range(len(users)):
+#         for user_second in range(user_first, len(users)):
+#             age1 = list_of_dicts[user_second].get('age')
+#             course1 = list_of_dicts[user_second].get('course')
+#             age2 = data.get('age')
+#             course2 = data.get('course')
+#             curr_sum_matching = ((1 / (age1 + age2)) * 500) + ((1 / (course1 + course2)) * 1000)
+#             final_data[f"{list_of_dicts[user_second].get('external_id')}"] = curr_sum_matching
+
+#     return final_data
+    
